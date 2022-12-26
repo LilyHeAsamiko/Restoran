8e0472154de4f2192b9558e92cae15c9da7a08ca
Qin He(Lily) <45560662+LilyHeAsamiko@users.noreply.github.com>
2022-12-26 07:36:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # flask
-
 from flask import Flask, render_template, request, session, redirect, url_for
 
 import requests
@@ -14,13 +13,21 @@ app.secret_key = 'BAD_SECRET_KEY'
 def index():
   return render_template("index.html")
  
-@app.route("/book")
-def book():
-  return render_template('book.html')
+@app.route("/about")
+def about():
+  return render_template('about.html')
 
-@app.route("/contact")
-def danci(chapter):  
-  return render_template('contact.html')
+@app.route("/menu")
+def menu(chapter):  
+  return render_template('menu.html')
+
+@app.route("/reservation")
+def reservation(chapter):  
+  return render_template('reservation.html')
+
+@app.route("/shop")
+def shop(chapter):  
+  return render_template('shop.html')
 
 # Details on the Secret Key: https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY
 # NOTE: The secret key is used to cryptographically-sign the cookies used for storing
